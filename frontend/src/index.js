888a4ef5b704b19c48b51237382e12b3b6310059
Nadezhda <n.d.niki2012@gmail.com>
2023-11-30 23:46:44 +0300

    update .eslintrc.yml, Makefile and index.js; add babel plugin and babel.config.json; delete styles.scss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
+import './index.css';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
